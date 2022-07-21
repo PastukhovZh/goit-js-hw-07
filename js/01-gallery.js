2363058ae2ccfc3a1ab946a213b5f8d9e9e5e89b
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-07-21 18:55:06 +0300

    Gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const destructImages = destruct => {
+    const { preview, original, description } = destruct;
+  return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+    </a>
+  </div>`
+}
 
-console.log(galleryItems);
+const galerryOfImages = document.querySelector('.gallery');
+const addImages = galleryItems.map(destructImages).join('');
+
+galerryOfImages.insertAdjacentHTML('afterbegin', addImages);
+
+const container = document.querySelector('.gallery')
+
+container.addEventListener('click', clicker)
+
+// const originalImageSize = basicLightbox.create(document.querySelector('[data-source]'))
+function clicker (evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+  return
+}
+
+
+
+  const originalImageSize = basicLightbox.create(evt.target.dataset.source)
+
+document.querySelector('.gallery').onclick = originalImageSize.show;
+
+  // return originalImageSize
+// console.log(originalImageSize)
+
+}
+
+
+// const originalImageSize = document.querySelector('[data-source]')
+// document.querySelector('body').onclick = originalImageSize.show;
+
+  
+// console.log(originalImageSize);
